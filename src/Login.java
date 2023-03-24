9be2e8dad18abc45d92b0327c92e3fc81efc188b
CasMonDavid <90111855+CasMonDavid@users.noreply.github.com>
2023-03-23 23:29:01 -0700

    Splash funcional y borre codigo residual

--- src/Login.java
+++ src/Login.java
@@ -26,114 +26,50 @@ public class Login extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
-		//SPLASH////////////////////////////////////////////////////////////////////
-		
-		
-		//splat
-		JPanel splat = new JPanel();
-		splat.setLayout(null);
-		splat.setBackground(Color.decode("#B3FFF1"));
-		splat.setVisible(true);
-		splat.setSize(500,700);
-		splat.setLocation(0,0);
-		this.add(splat);
-		
-		//inicio de sesion
-		JPanel inicio = new JPanel();
-		inicio.setLayout(null);
-		inicio.setBackground(Color.decode("#B3FFF1"));
-		inicio.setSize(500,700);
-		inicio.setLocation(0,0);
-		inicio.setVisible(true);
-		
-		//Una vez logeado el usuario le salta esta pantalla 
-		JPanel inicioUsuario = new JPanel();
-		inicioUsuario.setLayout(null);
-		inicioUsuario.setBackground(Color.decode("#B3FFF1"));
-		inicioUsuario.setSize(500,700);
-		inicioUsuario.setLocation(0,0);
-		inicioUsuario.setVisible(true);
-		
-		//Menu principal
-		JPanel menu = new JPanel();
-		menu.setLayout(null);
-		menu.setBackground(Color.decode("#B3FFF1"));
-		menu.setVisible(true);
-		menu.setSize(500,700);
-		menu.setLocation(0,0);
-		this.add(menu);
-		
-		//mi cuenta personal
-		JPanel cuenta = new JPanel();
-		cuenta.setLayout(null);
-		cuenta.setBackground(Color.decode("#B3FFF1"));
-		cuenta.setSize(500,700);
-		cuenta.setLocation(0,0);
-		cuenta.setVisible(true);
-		
-		//lista de usuarios
-		JPanel listaUs = new JPanel();
-		listaUs.setLayout(null);
-		listaUs.setBackground(Color.decode("#B3FFF1"));
-		listaUs.setSize(500,700);
-		listaUs.setLocation(0,0);
-		listaUs.setVisible(true);
-		
-		//ayuda
-		JPanel ayuda = new JPanel();
-		ayuda.setLayout(null);
-		ayuda.setBackground(Color.decode("#B3FFF1"));
-		ayuda.setVisible(true);
-		ayuda.setSize(500,700);
-		ayuda.setLocation(0,0);
-		this.add(ayuda);
-		
-		//panel de inicio de sesion
-
-		JLabel etiqueTitulo = new JLabel("Iniciar sesión",JLabel.CENTER);
-		etiqueTitulo.setFont(new Font("HELVETICA",Font.BOLD,35));
-		etiqueTitulo.setSize(230,32);
-		etiqueTitulo.setLocation(135,75);
-		inicio.add(etiqueTitulo);
-				
-		JLabel etiqueUsuario = new JLabel("Escriba su nombre:");
-		etiqueUsuario.setFont(new Font("Arial",Font.PLAIN,18));
-		etiqueUsuario.setSize(300,23);
-		etiqueUsuario.setLocation(100,150);
-		inicio.add(etiqueUsuario);
-		
-		JTextField nombreUsuario = new JTextField();
-		nombreUsuario.setSize(300,25);
-		nombreUsuario.setLocation(100,180);
-		inicio.add(nombreUsuario);
-		
-		JLabel etiqueContraseñaREG = new JLabel("Escriba su contraseña:");
-		etiqueContraseñaREG.setFont(new Font("Arial",Font.PLAIN,18));
-		etiqueContraseñaREG.setSize(300,23);
-		etiqueContraseñaREG.setLocation(100,210);
-		inicio.add(etiqueContraseñaREG);
-				
-		JPasswordField contraseñaINI = new JPasswordField();
-		contraseñaINI.setSize(300,25);
-		contraseñaINI.setLocation(100,240);
-		inicio.add(contraseñaINI);
-		
-		JButton iniciarSesionBtn = new JButton("Iniciar sesión");
-		iniciarSesionBtn.setSize(150,30);
-		iniciarSesionBtn.setLocation(175,320);
-		inicio.add(iniciarSesionBtn);
-		
-		
-		
-		
+		//INICIO DE LA APLICACION
+		splash();
 		
 		this.repaint();
 	}
 	
 	public void limpiarPaneles() {
 		
-		//if y cositas
+		if (panel!=null) {
+			this.remove(panel);
+		}
 		
+		if (actual.equals("splash")){
+			panel = splash(); 
+			
+			this.add(panel);
+			
+			this.repaint();
+			this.revalidate();
+		}
+		
+	}
+	
+	public JPanel splash() {
+		anterior = actual;
+		actual = "splash";
+		
+		//splash
+		JPanel splash = new JPanel();
+		splash.setLayout(null);
+		splash.setBackground(Color.decode("#B3FFF1"));
+		splash.setVisible(true);
+		splash.setSize(500,700);
+		splash.setLocation(0,0);
+		
+		try {
+			this.add(splash);
+			Thread.sleep(5000);
+			this.remove(splash);
+		}catch(InterruptedException ex) {
+			
+		}
+		
+		return splash;
 	}
 	
 	public JPanel pantallaSplash() {
