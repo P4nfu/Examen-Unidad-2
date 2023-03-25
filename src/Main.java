84317bb7479c8153814b98254b8b732a0f03cf02
P4nfu <125700222+P4nfu@users.noreply.github.com>
2023-03-24 20:16:02 -0700

    Jmenubar prueba
    JMenuBar

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import java.io.IOException;
 
 public class Main {
 
+<<<<<<< HEAD
 	public static void main(String[] args) throws IOException {
+=======
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+>>>>>>> parent of 7c611db (Prueba de push)
 		Login mi_ventana = new Login();
 	}
 
