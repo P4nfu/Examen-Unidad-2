9cc09e87a9314e3d1c2f534406cf9acbe6b37efe
P4nfu <125700222+P4nfu@users.noreply.github.com>
2023-03-26 18:01:36 -0700

    no push

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@ import java.io.IOException;
 
 public class Main {
 
-//<<<<<<< HEAD
-	public static void Main(String[] args) throws IOException {
-//=======
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//>>>>>>> parent of 7c611db (Prueba de push)
-		Login mi_ventana = new Login();
+	public static void main(String[] args) throws IOException {
+		Login ventana = new Login();
 	}
 
 }
