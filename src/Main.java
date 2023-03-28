206abf076139bcc9d894f985ad861d3efa753ed1
P4nfu <125700222+P4nfu@users.noreply.github.com>
2023-03-27 23:29:22 -0700

    Revert "Merge branch 'main' of https://github.com/P4nfu/Examen-Unidad-2"
    This reverts commit 4a87b25951058ba266ad426ff989982e02c86f7d, reversing
    changes made to 549a996e6c6e7542949ef8d81fa24e65d1ccc66d.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Ventana ventana = new Ventana();
+		Login ventana = new Login();
 	}
 
 }
